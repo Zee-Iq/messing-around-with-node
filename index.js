227b0263d5a190d431b5f7e20a302206381d3953
Zeeshan <zeeshan.iqbal@hotmail.de>
2022-02-10 14:17:15 +0100

    understanding node identifiers

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hello mama")
+/* Understanding the node runtime */
+
+/* 1. Understanding built in identifier called "global" */
+
+/* creating a constant that returns a random number and rounding up the floating point number to an integer with "ceil" */
+const luckyNum = (Math.ceil(Math.random()*100))
+
+global.luckyNum=luckyNum
+ console.log("global.luckyNum ", global.luckyNum);
