3bacb938783be15161ac780bfec46ad49a0cfba0
Zeeshan <zeeshan.iqbal@hotmail.de>
2022-02-10 14:40:02 +0100

    understanding node's eventloop

--- index.js
+++ index.js
@@ -9,13 +9,20 @@
 const luckyNum = Math.ceil(Math.random() * 100);
 
 global.luckyNum = luckyNum;
- console.log("global.luckyNum = luckyNum; ", global.luckyNum );
- /* expected result = global.luckyNum = luckyNum;  86 */
+console.log("global.luckyNum = luckyNum; ", global.luckyNum);
+/* expected result = global.luckyNum = luckyNum;  86 */
 
+/* 2. Understanding built in identifier called "process" */
+/* "process" gives acess to currently running process  */
 
+console.log(process.platform);
+/* When running node . -->  expected result = linux */
 
- /* 2. Understanding built in identifier called "process" */
- /* "process" gives acess to currently running process  */
+/* 3. Understanding Node's eventloop and understanding what "non-blocking" means
+ */
+/* Node implements an eventloop (just like a web browser) -->
+        Now node can push off intensive tasks/operations to another thread-->
+            This allow Node to keep the main thread free for intensive tasks
+                This is why Node is ideal for intensive tasks with high input  output at the same time like real time apps or web servers
 
- console.log(process.platform) 
- /* When running node . -->  expected result = linux */
+*/
