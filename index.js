e8aec267bb4b2417fcd638291168e5c479cf0df3
Zeeshan <zeeshan.iqbal@hotmail.de>
2022-02-10 14:18:41 +0100

    Understanding node built in identifiers --> "global"

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-/* Understanding the node runtime */
+/* Understanding the node runtime and it's identifiers */
 
 /* 1. Understanding built in identifier called "global" */
 
 /* creating a constant that returns a random number and rounding up the floating point number to an integer with "ceil" */
-const luckyNum = (Math.ceil(Math.random()*100))
+/* Assigning the constant luckyNum to--> global.luckyNum = luckyNum --> 
+    This allows the program to access the luckyNum from anywhere
+*/
+const luckyNum = Math.ceil(Math.random() * 100);
 
-global.luckyNum=luckyNum
- console.log("global.luckyNum ", global.luckyNum);
+global.luckyNum = luckyNum;
+console.log("global.luckyNum ", global.luckyNum);
+
+
